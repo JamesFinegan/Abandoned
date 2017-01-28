8a4d982110f9d4cb0c875e01eeca5d6bb2e4650d
jackson8en <jackson.8en@gmail.com>
2017-01-28 22:32:26 +0000

    First Prototype
    First Build of Fog, Model and pussy patrol

---
+++ Abandoned - Toy Store/Assets/TorchFlicker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchFlicker : MonoBehaviour {
+
+	public Light flickerLight;
+	public float timer;
+	public float normTime;
+	public float normIntensity;
+	public float highIntensity;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (FlickeringLight ());
+	}
+
+	IEnumerator FlickeringLight(){
+		flickerLight.intensity = normIntensity;
+		timer = Random.Range (normTime/2, normTime);
+		yield return new WaitForSeconds (timer);
+		flickerLight.intensity = highIntensity;
+		timer = Random.Range (0.05f, 0.1f);
+		yield return new WaitForSeconds (timer);
+		if (Mathf.RoundToInt (Random.value) == 1) {
+			flickerLight.intensity = 0;
+		}
+		timer = Random.Range (0.5f, 1.5f);
+		yield return new WaitForSeconds (timer);
+		StartCoroutine (FlickeringLight());
+	}
+}
